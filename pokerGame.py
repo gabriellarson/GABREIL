280236f1bea67005d5a420a3667e814788153cdf
Gabriel Larson <55459720+gabriellarson@users.noreply.github.com>
2023-01-22 21:16:24 -0600

    working on it

--- pokerGame.py
+++ pokerGame.py
@@ -1,15 +1,48 @@
+import random
+import pokerHand
+import pokerPlayer
+
 class pokerGame:
 
-    def __init__(self, players, startingBB, handsPerLevel):
+    def __init__(self, players, startingChips, handsPerLevel):
         self.players = players
-        self.startingBB = startingBB
+        self.startingChips = startingChips
         self.handsPerLevel = handsPerLevel
-        self.deck = ["As","2s","3s","4s","5s","6s","7s","8s","9s","Ts","Js","Qs","Ks","Ad","2d","3d","4d","5d","6d","7d","8d","9d","Td","Jd","Qd","Kd","Ac","2c","3c","4c","5c","6c","7c","8c","9c","Tc","Jc","Qc","Kc","Ah","2h","3h","4h","5h","6h","7h","8h","9h","Th","Jh","Qh","Kh"]
+        self.deck = ["As","2s","3s","4s","5s","6s","7s","8s","9s","Ts","Js","Qs","Ks"
+                    ,"Ad","2d","3d","4d","5d","6d","7d","8d","9d","Td","Jd","Qd","Kd"
+                    ,"Ac","2c","3c","4c","5c","6c","7c","8c","9c","Tc","Jc","Qc","Kc"
+                    ,"Ah","2h","3h","4h","5h","6h","7h","8h","9h","Th","Jh","Qh","Kh"]
 
-    def deal():
-        deck.shuffle()
-        for player in players:
-            player.hand.append(deck.pop())
-            player.hand.append(deck.pop())
-            player.hand.sort()
-            print(player.name, player.hand)
+
+def main():
+    list = [pokerPlayer("Player1"),
+            pokerPlayer("Player2"),
+            pokerPlayer("Player3"),
+            pokerPlayer("Player4"),
+            pokerPlayer("Player5"),
+            pokerPlayer("Player6"),
+            pokerPlayer("Player7"),
+            pokerPlayer("Player8"),
+            pokerPlayer("Player9")]
+
+    game = pokerGame(list, 100, 20)
+
+    for player in game.players:
+        player.setChips(game.startingChips)
+    
+    BBsize = game.startingChips/100
+    
+    dealerPosition = random.randint(0,len(game.players)-1)
+
+    while(len(game.players) > 3):
+        for hand in range(game.handsPerLevel):
+            hand = pokerHand(game.deck, game.players, dealerPosition, BBsize, False)
+            hand.playHand()
+        BBsize *= 2
+        
+
+if __name__ == "__main__":
+    main()  
+
+
+    
--- pokerPlayer.py
+++ pokerPlayer.py
@@ -1,3 +1,37 @@
 class pokerPlayer:
-    def __init__(self, chips):
+    def __init__(self, name):
+        self.name = name
+        self.chips = None
+        self.hand = [None,None]
+        self.previousAction = None
+
+    def deltaChips(self, chipsDelta):
+        self.chips += chipsDelta
+
+    def setChips(self, chips):
         self.chips = chips
+
+    def getChips(self):
+        return self.chips
+
+    def setHand(self, card1, card2):
+        self.hand = [card1, card2]
+        self.hand.sort()
+    
+    def getHand(self):
+        return self.hand
+
+    def getName(self):
+        return self.name
+
+    def takeAction(self, gamestate):
+        action = "fold"
+        self.appendAction(action)
+        return action
+
+    def getPreviousAction(self):
+        return self.previousAction
+
+    def appendAction(self, action):
+        self.previousAction.append(action)
+    
